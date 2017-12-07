4f26214e66ff9f340d771fd4eb4ce7d9b9545ae5
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-07 15:56:07 +0100

    Add standard files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+function drawTree(treeHeight) {
+    for (var i = 0 ; i < treeHeight ; i++) {
+        var star = '';
+        for (var j = 0 ; j <= i ; j++) {
+            star += '*';
+        }
+    console.log(star);
+    }
+}
+
+drawTree(5);
