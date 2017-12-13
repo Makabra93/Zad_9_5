71ea85af2018a5c4ca1400af3e018a24ae16be47
Mateusz Ławczys <ma6tek@gmail.com>
2017-12-13 01:14:05 +0100

    Fix cosmetics

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-
-
 var buttons = document.getElementsByClassName('button');
 var buttonLength = buttons.length;
 
 
-    for (var a = 0 ; a < buttonLength ; a++) {
+for (var i = 0; i < buttonLength; a++) {
     var buttonText = buttons[a].innerText;
     console.log('button ' + a + ' text is ' + buttonText);
 }
