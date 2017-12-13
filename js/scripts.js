c8b0c46f0b39f08dedd35ea474149cb21ae3e25b
Mateusz Ławczys <ma6tek@gmail.com>
2017-12-13 01:15:48 +0100

    Fix spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var buttons = document.getElementsByClassName('button');
 var buttonLength = buttons.length;
 
-
 for (var i = 0; i < buttonLength; a++) {
     var buttonText = buttons[a].innerText;
     console.log('button ' + a + ' text is ' + buttonText);
